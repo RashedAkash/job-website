c404511d0a9a58f2f1b33e184386bcad55225855
hablu <131282586+hablu@users.noreply.github.com>
2023-11-04 20:34:54 +0600

    footer create and responsive complete

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Footer/Footer';
 
 const MainLayouts = () => {
   return (
-    <div>
+    <div className=' max-w-5xl mx-auto overflow-hidden'>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
