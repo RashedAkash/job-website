8aff043b151c5d39843ca921262c0313e3bd2ecc
hablu <131282586+hablu@users.noreply.github.com>
2023-11-05 09:55:38 +0600

    brand section create and responsive create

---
+++ src/Components/Brand/Brand.jsx
@@ -0,0 +1,26 @@
+import Marquee from "react-fast-marquee";
+import brand1 from '../../assets/brand1.png'
+import brand2 from '../../assets/brand2.png'
+import brand3 from '../../assets/brand3.png'
+import brand4 from '../../assets/brand4.png'
+import brand5 from '../../assets/brand5.png'
+import brand6 from '../../assets/brand6.png'
+
+const Brand = () => {
+  return (
+    <div>
+       <Marquee>
+        <div className="bg-blue-700 py-4 gap-2 flex">
+          <img src={brand1} alt="" />
+    <img src={brand2} alt="" />
+    <img src={brand3} alt="" />
+    <img src={brand4} alt="" />
+    <img src={brand5} alt="" />
+    <img src={brand6} alt="" />
+    </div>
+  </Marquee>
+    </div>
+  );
+};
+
+export default Brand;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
+import Brand from '../../Components/Brand/Brand';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Brand />
     </div>
   );
 };
