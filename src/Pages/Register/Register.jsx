a7e293dac5636657beff4ac46ca78571f3c71540
hablu <131282586+hablu@users.noreply.github.com>
2023-11-05 07:30:13 +0600

    firebase connect and env file create

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -18,6 +18,13 @@ const Register = () => {
 				<a rel="noopener noreferrer" href="#">Forgot Password?</a>
 			</div>
 		</div>
+		<div className="space-y-1 text-sm">
+			<label  className="block dark:text-gray-400">Photo Url</label>
+			<input type="text" name="photo" id="photo" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 border focus:dark:border-violet-400" />
+			<div className="flex justify-end text-xs dark:text-gray-400">
+				<a rel="noopener noreferrer" href="#">Forgot Password?</a>
+			</div>
+		</div>
 		<button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400">Sign up</button>
 	</form>
 	<div className="flex items-center pt-4 space-x-1">
