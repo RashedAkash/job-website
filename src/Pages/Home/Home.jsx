ee9ce8e7280d3824ab486c1323bd13c51b57820e
hablu <131282586+hablu@users.noreply.github.com>
2023-11-05 10:25:22 +0600

    price area create and responsive complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import Brand from '../../Components/Brand/Brand';
+import Price from '../../Components/Price/Price';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Price />
       <Brand />
     </div>
   );
